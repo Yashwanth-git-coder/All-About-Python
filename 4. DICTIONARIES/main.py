60b590108eb67fbde366efd35a3ee0cf7fc77a94
Yashwanth <yashwanth7780@gmail.com>
2024-05-09 17:40:04 +0530

    Updated

---
+++ 4. DICTIONARIES/main.py
@@ -0,0 +1,16 @@
+"""
+ Working with Dictionaries
+"""
+
+alien_0 = {'color': 'green', 'points': 5}
+
+"""
+ Accessing Values in a Dictionary
+"""
+
+print(alien_0['color'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points !")
+
+
